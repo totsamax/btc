e8c8dc4cf66b8189cec2666f4413a94cb7ce78e2
Maxim <totsamax@gmail.com>
2015-07-25 18:34:13 +0300

    asd4

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ionic.keyboard": "1.0.4",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "org.apache.cordova.vibration": "0.3.13",
+    "org.apache.cordova.console": "0.2.13"
+}
+// BOTTOM OF METADATA
+});
